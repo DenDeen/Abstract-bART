e1a0b3d008bbcfbf2839fb7a8009b2e6748b3a72
Vaishnav Dilip <100198464+vaishnavdilip@users.noreply.github.com>
2023-02-22 00:55:57 +0100

    Added inputting code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from utils import *
 from urllib import response
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 import requests
 import json
 
@@ -10,6 +12,10 @@ artworks_app = App(uri="bolt://54.92.211.62:7687", username = "neo4j", password=
 driv = artworks_app.create_driver()
 
 # print(driv.run("MATCH (n) RETURN n LIMIT 25"))
+
+upload_folder = os.path.join('static', 'uploads')
+
+app.config['UPLOAD'] = upload_folder
 
 @app.route("/") 
 def index():
@@ -25,9 +31,15 @@ def index():
 def search():
     return None
 
-@app.route('/style-transfer')
-def style_transfer():
 
+@app.route('/style-transfer', methods=['GET', 'POST'])
+def style_transfer():
+    if request.method == 'POST':
+        file = request.files['img']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD'], filename))
+        img = os.path.join(app.config['UPLOAD'], filename)
+        return render_template('style-transfer.html', img=img)
     return render_template('style-transfer.html')
 
 app.run(host="0.0.0.0", port=80, debug=True)
